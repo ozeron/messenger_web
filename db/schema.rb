39fd6df2f3d19c05db2fa1e1dbca3a1d59da9f9d
Oleksandr Lapchenko <ozeron@me.com>
2015-10-26 03:53:19 +0200

    Added messages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151025131421) do
+ActiveRecord::Schema.define(version: 20151026015125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,13 @@ ActiveRecord::Schema.define(version: 20151025131421) do
     t.integer  "parent_id"
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string   "theme"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "node_groups", force: :cascade do |t|
