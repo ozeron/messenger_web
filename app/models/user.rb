58ce5b383462d15644a9a2b32c5233beae051769
Oleksandr Lapchenko <ozeron@me.com>
2015-10-28 13:51:00 +0200

    Added main localization files

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,12 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  LANGUAGES = [['English','en'], ['Русский','ru'], ['Українська','uk']]
+
+  validates :language,
+            presence: true,
+            inclusion: {in: LANGUAGES.map(&:second)}
+
   def vk_app
     @app ||= ApiWrapper::VkFabric.build(connection_parameters['vk'])
     if connection_parameters['vk']['access_token'] != @app.access_token
