992d7c0250afd0cde1eccca4b0e9c764565346be
Oleksandr Lapchenko <ozeron@me.com>
2015-11-06 13:04:25 +0200

    Removed N+1 queries from MassMailings

--- app/boards/mass_mailings_board.rb
+++ app/boards/mass_mailings_board.rb
@@ -2,11 +2,12 @@ class MassMailingsBoard < BasicBoard
   attr_accessor :mass_mailing
 
   def mass_mailings
-    @mass_mailings ||= MassMailing.all.order(id: :desc)
+    @mass_mailings ||= MassMailing.includes(:nodes, :message, mass_mailing_nodes: :node)
+                       .all.order(id: :desc)
   end
 
   def mass_mailing
-    @mass_mailing ||= params[:id] ? MassMailing.includes(mass_mailing_nodes: :node).find(params[:id]) : MassMailing.new
+    @mass_mailing ||= params[:id] ? MassMailing.includes(:nodes, mass_mailing_nodes: :node).find(params[:id]) : MassMailing.new
   end
 
   def mass_mailing_nodes_ids
