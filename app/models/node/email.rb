2a25e64ae5c6cf7a5040c75f83507e3d270b5346
Oleksandr Lapchenko <ozeron@me.com>
2015-11-06 10:48:19 +0200

    Added description to email nodes

--- app/models/node/email.rb
+++ app/models/node/email.rb
@@ -1,4 +1,5 @@
 class Node::Email < Node
+  delegate :telephones, :address, :emails, :website, :city, to: :options
   def self.build(attributes)
     attributes['options'] = attributes.extract!('email') || {}
     new(attributes)
@@ -9,6 +10,18 @@ class Node::Email < Node
   end
 
   def email
-    options['email']
+    options.key?('email') ? options['email'] : options['emails'].first
+  end
+
+  def options
+    return @options_mash if @options_mash
+    @options_mash = Hashie::Mash.new(self['options'])
+    @options_mash.default = ''
+    @options_mash
+  end
+
+  def description
+    self['description'] + "\n" +
+    "тел. #{telephones.try(:join, ',')}\nаддрес: #{address}\nemail: #{email}\nwebsite: #{website}\nмісто: #{city}".html_safe
   end
 end
