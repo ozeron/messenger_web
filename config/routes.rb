3130b74602ab73607ede798b9d45500aeec39e83
Oleksandr Lapchenko <ozeron@me.com>
2015-11-17 22:22:17 +0200

    Removed active Admin

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,11 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
+
   devise_scope :user do
     get "cabinet" => "users/registrations#show"
   end
-  devise_for :admin_users, ActiveAdmin::Devise.config
 
-  ActiveAdmin.routes(self)
   resources :groups
   resources 'nodes'
   resources 'messages'
