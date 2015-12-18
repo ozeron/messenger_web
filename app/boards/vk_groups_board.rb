52779acaf62fe219615bb1a507f5beac38dfba1e
Oleksandr Lapchenko <ozeron@me.com>
2015-12-18 02:31:33 +0200

    Improvments

--- app/boards/vk_groups_board.rb
+++ app/boards/vk_groups_board.rb
@@ -7,14 +7,19 @@ class VkGroupsBoard < BasicBoard
     if params[:query].blank?
       @groups ||= []
     else
-      @groups ||= app.groups.search(q: params[:query], fields: %w(description members_count))
-                  .fetch('items')
-                  .map do |g|
+      raw = app.groups.search(q: params[:query], fields: %w(description members_count))
+                .fetch('items')
+      ids = raw.map { |h| h['id'] }
+      request = app.groups.get_by_id(group_ids: ids, fields: ['description', 'can_post'])
+      records = request.map do |g|
                     g['members_count'] ||= 'unknown'
                     g['description'] ||= ''
                     g
-                  end.map { |g| Hashie::Mash.new(**g.symbolize_keys) }
+                  end
+      filtered = records.select { |h| h['can_post'] == 1 }
+      @groups = filtered.map { |g| Hashie::Mash.new(**g.symbolize_keys) }
     end
+
   end
 
 end
