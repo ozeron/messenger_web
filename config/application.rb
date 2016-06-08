0894f081842b8ee98da604d6a5364e186d69d6da
server <simple_user@i.ua>
2016-06-08 21:42:12 +0300

    Font fixes

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ module Messenger
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
     config.time_zone = 'Kyiv'
+    config.assets.paths << Rails.root.join("vendor/assets/twitter-bootstrap-static/twitter/fonts/")
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*', '*.{rb,yml}').to_s]
