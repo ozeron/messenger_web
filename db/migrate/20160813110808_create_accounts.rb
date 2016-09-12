7702ae0953cb6184ab840d28033aef48aaf2d6a1
Oleksandr Lapchenko <ozeron@me.com>
2016-09-13 01:26:55 +0300

    Fixed

--- db/migrate/20160813110808_create_accounts.rb
+++ db/migrate/20160813110808_create_accounts.rb
@@ -1,9 +1,17 @@
 class CreateAccounts < ActiveRecord::Migration
-  def change
+  def up
     create_table :accounts do |t|
       t.json :connection_parameters, default: {}
       t.string :type
       t.timestamps null: false
     end
+
+    User.all.each do |user|
+      Account.create(connection_parameters: user.connection_parameters)
+    end
+  end
+
+  def down
+    drop_table :accounts
   end
 end
